70acadd40dde2b51524e1072f18e363a8f1df1a4
ModelZ <model5053@gmail.com>
2025-05-18 06:11:54 +0700

    add defination of damage calculations

--- wicked-waifus-game-server/src/logic/handler/mzcustomdamage.rs
+++ wicked-waifus-game-server/src/logic/handler/mzcustomdamage.rs
@@ -1,24 +1,68 @@
 use crate::{config, logic::player::Player};
 use rand::Rng;
 
-// If Cartetyia do damage
 pub struct Cartetyia {
+    // basic info
     pub atk: i32,
     pub hp: i32,
-    pub weapon_atk: i32,
+    pub weapon_atk: i32, 
+    pub enemy: Enemy, // enemy for calculation
+
+    // multiplier
+    pub ability_damage: f32, //ability damage multiplier
+    pub damage_bonus: f32, //increase damage bonus (Areo Damage Bonus, Normal Attack Bonus, etc)
+    pub damage_amplify: f32, //damage amplify (Damage Amplify, etc) same as final damage
+    pub special_damage: f32, //special damage multiplier
+    pub crit_damage: f32, 
 }
+
+pub struct Enemy {
+    // basic info
+    pub name: String,
+    pub level: i32,
+    pub hp: i32,
+    pub def: i32,
+    pub element_res: f32,
+
+    // multiplier
+    pub def_multiplier: f32,
+    pub element_res_multiplier: f32,
+    pub damage_taken_multiplier: f32,
+}
+
 
 impl Cartetyia {
     pub fn new() -> Self {
         // Load the configuration (if needed)
         //let config = config::get_config();
 
-        // Define Cartetyia stats
-        Self {
+        // Create a new enemy instance
+        let enemy = Enemy {
+            name: "Lorelei".to_string(),
+            level: 90,
+            hp: 558_562,
+            def: 1512,
+            element_res: 0.1,
+
+            def_multiplier: 1.0,
+            element_res_multiplier: 1.0,
+            damage_taken_multiplier: 1.0,
+        };
+
+        // Return a new instance of Cartetyia
+        return Self {
             atk: 312,
             hp: 14800,
             weapon_atk: 412,
-        }
+            enemy, // store the enemy in the struct
+
+            ability_damage: 1.0, //ability damage multiplier
+            damage_bonus: 1.0, //increase damage bonus (Areo Damage Bonus, Normal Attack Bonus, etc)
+            damage_amplify: 1.0, //damage amplify (Damage Amplify, etc) same as final damage
+            special_damage: 1.0, //special damage multiplier
+            crit_damage: 1.5, 
+        };
+        
     }
 
     pub fn if_cartethyia_dmg(player: &mut Player, damage: &mut i32) {
